241331b81cb2bb6fdd12cc3e8127a99c4d0e28a9
Jakesnake523 <unconfigured@null.spigotmc.org>
2019-10-31 21:53:32 -0500

    Update website

--- js/main.js
+++ js/main.js
@@ -9,10 +9,14 @@ $(document).ready(function (){
     $window.scroll(function() {
         if($window.scrollTop() >= stickydock.top){
             $('#nav').addClass('is-fixed-top');
+            $('.nav-placeholder').addClass('is-shown');
             $(".navbar-menu").addClass('navbar-dark');
+            $(".dropdown").removeClass('is-up')
         } else {
             $('#nav').removeClass('is-fixed-top');
+            $('.nav-placeholder').removeClass('is-shown');
             $(".navbar-menu").removeClass('navbar-dark');
+            $(".dropdown").addClass('is-up')
         }
     });
 });
@@ -20,13 +24,13 @@ $(document).ready(function (){
 function switchmode(){
     if (dark){
         dark = false;
-        setP('--background-color', 'white')
+        setP('--background-color', '#4E98BB')
         setP('--background-secondary', "white");
         setP('--background-third', "#FBFBFB");
         setP('--box-color', "white")
         setP('--text-color', "black")
-        setP('--background-image', "url('../img/2_city_light.jpg')");
-        
+        setP('--background-image', "url('../img/3_city_light.jpg')");
+        $('.splash').removeClass('dark');
         $(".dark-mode-icon").removeClass('fa-sun');
         $(".dark-mode-icon").addClass('fa-moon');
     } else {
@@ -35,14 +39,15 @@ function switchmode(){
         setP('--background-secondary', "#3B4048");
         setP('--background-third', "#393e45");
         setP('--box-color', "#393e46");
+        primary:#073652
         */
-        setP('--background-color', '#073652');
+       $('.splash').addClass('dark');
+        setP('--background-color', '#053853');
         setP('--background-secondary', "#084466");
         setP('--background-third', "#0b4769");
         setP('--box-color', "#09476b");
         setP('--text-color', "white");
-        setP('--background-image', "url('../img/2_city.jpg')");
-
+        setP('--background-image', "url('../img/2_city.jpg'),url('../img/snowcircles.png')");
         $(".dark-mode-icon").removeClass('fa-moon');
         $(".dark-mode-icon").addClass('fa-sun');
     }
